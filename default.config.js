fb0689edf8b8c940451f6a46ccc90783bfa78f29
Alexandre Saison <asaison@iut.univ-paris8.fr>
2017-11-22 22:38:14 +0100

    Bump version 0.0.1  - Initial commit  - created some new Tests to handle travis support  - created .travis.yml  - added helpers function to handle simple functions for Application class  - created services folder    - created SSEService class    - move RequestService into services folder  - eslintrtc to handle good programming convention  - package.json to better handle test (mocha on dist)  - babelrc to be on preset es2015

--- default.config.js
+++ default.config.js
@@ -1,11 +1,12 @@
 export const config= {
   port: 3000,
+  env: 'dev'
 };
 
-export const AVAILABLE_METHODS= {
-  GET: "get",
-  POST: "post",
-  PUT: "put",
-  DELETE: "delete",
-  ANY: "all",
+export const AVAILABLE_METHODS = {
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DELETE: 'delete',
+  ANY: 'all',
 };
