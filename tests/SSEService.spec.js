b854f6ba4334594f37eede3fd683c658c3e31c0f
Alexandre Saison <alexandre.saison@omnilog.fr>
2017-11-23 08:29:00 +0100

    [APPLICATION][TRAVIS] added travis support + specs
    + : created some new Tests to handle travis support
    + : created .travis.yml

---
+++ tests/SSEService.spec.js
@@ -0,0 +1,9 @@
+import {SSEService} from '../src/services/SSEService';
+import {it,describe} from 'mocha';
+
+describe('#SSEService', function() {
+  it('should success at anytime', function() {
+    const sseService = new SSEService('http://api.github.com', 'onKeyDown', 'onNewMessage');
+    return sseService !== null;
+  })
+});
