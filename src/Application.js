793b64e251ef38a1ab026828a09de6521241e375
Alexandre Saison <alexandre.saison@omnilog.fr>
2017-11-24 10:22:51 +0100

    Bump version 0.0.2
    - Creation of Store
    - Creation of User
    - update helpers.js
    - add get + post request on RequestService
    - Index for import using a Single File
    - Every Service extends BasicService to simplify implementation
    - added POST request for RequestService
    - update SSEServcice to handle new future Store
    - Store : creation of Store Model to handle data to the all Application
    - helpers : Handling body_parser for POST Request and Store
    - Authentication : Created the Authentication Service that the Store need to handle which User is Connected
    - added API_KEY from googleAPI to default.config
    - update route on index.js
    - added chai and googleapis
    - update Application
      - handle getServices
      - handle services function
    - added GoogleService to exports of Services
    - Test about GoogleService and Application
    - refactoring to try to handle test from travis

--- src/Application.js
+++ src/Application.js
@@ -1,27 +1,72 @@
-import {config, AVAILABLE_METHODS} from "../default.config";
-import {__init,app} from './helpers';
-
+import {config} from "./default.config";
+import {__init, app, newRoute} from './helpers';
+import {Store} from './Store';
+import {AuthenticationService, GoogleService} from './services';
+import {User} from './User';
 
 export class Application {
   constructor () {
     this.app =  app();
+    this._store = null;
+    this._services = new Map();
     __init(this.app);
+    this.init(new User(1405006118, 'badhrc'));
   }
-
+  
+  
+  /**
+   * get Storage
+   * @returns {null|Store}
+   */
+  get store () {
+    return this._store;
+  }
+  
+  addService(serviceName, service) {
+    this._services.set(serviceName, service);
+  }
+  
+  getService(name) {
+    return this._services.get(name)
+  }
+  
+  
   /**
    * Start the API application
    */
   run() {
     this.app.listen(config.port, function() {
-      console.log(`Running at 0.0.0.0:${config.port}`);
+      console.log(`Running at 0.0.0.0 :${config.port}`);
     });
   }
-
-  handleGetRequest(route, method, callback) {
-    if(!AVAILABLE_METHODS.hasOwnProperty(method.toUpperCase())){
-      throw new Error(`Method ${method} is unavailable !`);
-    }
-
-    this.app.call(method, callback, route);
+  
+  
+  /**
+   * Init the store and the Authentication Service with the User id => UID
+   * @param user
+   */
+  init(user) {
+    const uid = user.uid;
+    this._store = new Store(null, uid);
+    this.addService('Auth', new AuthenticationService(this.store));
+    this.addService('Google', new GoogleService(this.store));
+  }
+  
+  /**
+   * Handle a GET request from Application into express API
+   * @param {String} route
+   * @param {Function} callback the function to call at least
+   */
+  get(route, callback) {
+    return this.app.get(route, callback);
+  }
+  
+  
+  post(route, callback) {
+    return this.app.post(route, callback);
+  }
+  
+  static route(path) {
+    return newRoute(path)
   }
 }
---
+++ src/services/index.js
@@ -0,0 +1,12 @@
+import {AuthenticationService} from './AuthenticationService';
+import {RequestService} from './RequestService';
+import {SSEService} from './SSEService';
+import {GoogleService} from './GoogleService';
+
+
+export {
+  AuthenticationService,
+  RequestService,
+  SSEService,
+  GoogleService
+}
