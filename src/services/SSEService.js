d4cb87911a3fb4c0a61ef3ded3f3cd3b3f3aad49
Alexandre Saison <alexandre.saison@omnilog.fr>
2017-11-28 20:37:50 +0100

    [API][SERVICE][SSE] handling Store + Error emitting + socket.io
    - Socket.io implementation
    - error emit implementation
    - listening on port 4200 (FIX: will need to listen on config.SOCKET_IO_PORT soon)
    - constructor to handle setting store if defined in args

--- src/services/SSEService.js
+++ src/services/SSEService.js
@@ -1,9 +1,17 @@
 import {BasicService} from './BasicService';
+import {Store} from '../Store';
 
 export class SSEService extends BasicService{
 
   constructor(pathSSE, ...args) {
     super(null);
+    if(args.find(arg => arg instanceof Store) !== null) {
+      this.store = new Store(args.find(arg => arg instanceof Store), null);
+      console.log('[SSE] store = ', this.store);
+    } else {
+      console.log('[SSE] no Store defined');
+      this.store = new Store();
+    }
     if(!pathSSE) {
       throw new Error("path from listener is missing !");
     }
@@ -20,12 +28,13 @@ export class SSEService extends BasicService{
     console.log(`MSG => ${msg}`);
   }
   
-  static handleError(err) {
+  static handleError(err, socket) {
+    socket.emit('error_sse', {type: 'SSE', err: err});
     throw new Error(err.message);
   }
   
   start() {
-    const io = require('socket.io')(3000);
+    const io = require('socket.io')(4200);
     io.on('connection', function(socket) {
       socket.emit('start', {});
     })
