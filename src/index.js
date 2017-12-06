902beef7b465b6e58730f8b0836fbdbc1f6535c3
Alexandre Saison <alexandre.saison@omnilog.fr>
2017-12-06 14:19:53 +0100

    [APPLICATION][SSL] set application to handle HTTP2
     - added certificate to createServer from HTTP2 in Application

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ const app = new Application();
 const githubRouter = require('./routers/GithubRouter')(app);
 const googleRouter = require('./routers/GoogleRouter')(app);
 
-app.run();
-
 /**
  *  ROUTES HANDLERS
  */
@@ -15,3 +13,22 @@ app.use(googleRouter);
 app.get('/', function(req,res) {
   res.redirect('https://documenter.getpostman.com/view/1161028/rand-ia-api-2017/7EK5qh1');
 });
+
+app.get('/events', function (req, res) {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  
+  // send a ping approx every 2 seconds
+  const timer = setInterval(() => {
+    res.write('data: ping\n\n');
+    
+    // !!! this is the important part
+    res.flush()
+  }, 2000);
+  
+  res.on('close', function () {
+    clearInterval(timer)
+  })
+});
+
+app.run();
